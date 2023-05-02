d952ea8748ede057ee5f54f89d0ec645b1cc4f61
celio <celiofarroco@gmail.com>
2023-05-02 19:08:05 -0300

    Add list tags

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const TagsController = require("../controllers/TagsController");
+const tagsRoutes = Router();
+
+const tagsControler = new TagsController();
+
+tagsRoutes.get("/:user_id", tagsControler.index);
+
+module.exports = tagsRoutes;
